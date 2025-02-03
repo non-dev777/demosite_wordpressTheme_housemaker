d86ca7c20300da78d2d734392dea2b03c147cd77
non-dev777 <nobu.ken.777@gmail.com>
2025-02-03 14:11:04 +0900

    ✨ Add Japanese font (Shippori Mincho) and update font settings

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<meta name="description" content="サイトディスクリプションが入ります。">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Shippori+Mincho:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 <body>

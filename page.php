c9ef8e37d932dd39c4cd7e935387b5479b82777b
non-dev777 <nobu.ken.777@gmail.com>
2025-02-03 11:17:29 +0900

    固定ページ（page.php）を修正し、コンタクトフォーム用のcss設定

--- page.php
+++ page.php
@@ -2,7 +2,12 @@
 
 <?php if (have_posts()) : ?>
 	<?php while (have_posts()) : the_post(); ?>
-		<?php the_content(); ?>
+	<section class="py-5">
+		<div class="container">
+			<h2 class="fw-bold mb-4 text-center"><?php the_title(); ?></h2>
+			<?php the_content(); ?>
+		</div>
+	</section>
 	<?php endwhile; ?>
 <?php endif; ?>
 
